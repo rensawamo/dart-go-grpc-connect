ea426c6950bbdfffe1c23f2d0fa16a75d0ddf870
rensawamo <ren0112ngzk@gmail.com>
2025-02-06 15:01:29 +0900

    feat: chat screen

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,125 +1,158 @@
+import 'package:eliza/gen/eliza.pb.dart';
 import 'package:flutter/material.dart';
+import 'package:connectrpc/http2.dart';
+import 'package:connectrpc/connect.dart';
+import 'package:connectrpc/protobuf.dart';
+import 'package:connectrpc/protocol/connect.dart' as protocol;
+import './gen/eliza.connect.client.dart';
+
+final transport = protocol.Transport(
+  baseUrl: "https://demo.connectrpc.com",
+  codec: const ProtoCodec(), // Or JsonCodec()
+  httpClient: createHttpClient(),
+);
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ElizaApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ElizaApp extends StatelessWidget {
+  const ElizaApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Eliza',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: ChatPage(transport: transport),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class ChatPage extends StatefulWidget {
+  const ChatPage({super.key, required this.transport});
+  final Transport transport;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<ChatPage> createState() => _ChatPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ChatPageState extends State<ChatPage> {
+  final messages = List<({String sentence, bool byUser})>.empty(growable: true);
+  final currentSentence = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void addMessage(String sentence, bool byUser) {
+    setState(() => messages.add((sentence: sentence, byUser: byUser)));
+  }
+
+  void send(String sentence) async {
+    addMessage(sentence, true);
+    final response = await ElizaServiceClient(widget.transport).say(
+      SayRequest(sentence: sentence),
+    );
+    addMessage(response.sentence, false);
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                child: ListView(
+                  children: [
+                    for (final message in messages)
+                      Column(
+                        key: ObjectKey(message),
+                        children: [
+                          if (message.byUser) ...[
+                            const Row(
+                              children: [
+                                Spacer(),
+                                Text(
+                                  "You",
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                )
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                const Spacer(),
+                                Text(
+                                  message.sentence,
+                                  textAlign: TextAlign.left,
+                                ),
+                              ],
+                            )
+                          ] else ...[
+                            const Row(
+                              children: [
+                                Text(
+                                  "Eliza",
+                                  style: TextStyle(
+                                    color: Colors.blue,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                                Spacer(),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  message.sentence,
+                                  textAlign: TextAlign.left,
+                                ),
+                                const Spacer(),
+                              ],
+                            )
+                          ]
+                        ],
+                      )
+                  ],
+                ),
+              ),
+              Row(
+                children: [
+                  Flexible(
+                    child: TextField(
+                      controller: currentSentence,
+                      decoration: const InputDecoration(
+                        hintText: 'Write your message...',
+                        border: UnderlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      final sentence = currentSentence.text;
+                      if (sentence.isEmpty) {
+                        return;
+                      }
+                      send(sentence);
+                      currentSentence.clear();
+                    },
+                    child: const Text(
+                      'Send',
+                      style: TextStyle(color: Colors.blue),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
