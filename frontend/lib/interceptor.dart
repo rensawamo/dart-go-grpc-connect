34fc1b386fb788eee325816716b9aa996ce5fb1f
rensawamo <ren0112ngzk@gmail.com>
2025-02-06 15:43:43 +0900

    feat: interceptor に戻り値の出力を追加

--- frontend/lib/interceptor.dart
+++ frontend/lib/interceptor.dart
@@ -1,8 +1,12 @@
 import 'package:connectrpc/connect.dart';
 
-final Interceptor interceptor = <I extends Object, O extends Object>(next) {
-  return (req) {
+final Interceptor interceptor = <I extends Object, O extends Object>(next) { // 戻り値も見れる
+  return (req) async {
     print("sending message to ${req.url}");
-    return next(req);
+    final res = await next(req);
+    if (res is UnaryResponse<I, O>) {
+      print(res.message);
+    }
+    return res;
   };
 };
