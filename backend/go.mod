eaa30a9e76ef58db2f1ccd99f6cf83af39aeb4b1
rensawamo <ren0112ngzk@gmail.com>
2025-02-07 14:50:03 +0900

    feat: backend buf gen

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module eliza
+
+go 1.23.3
--- frontend/lib/transport/grpc_transport.dart
+++ frontend/lib/transport/grpc_transport.dart
@@ -1,16 +1,13 @@
 import 'package:connectrpc/connect.dart';
 import 'package:connectrpc/http2.dart';
 import 'package:connectrpc/test.dart';
+import 'package:connectrpc/protobuf.dart';
+import 'package:connectrpc/protocol/grpc.dart' as grpc;
 import 'package:eliza/gen/eliza.connect.spec.dart';
 import 'package:eliza/gen/eliza.pb.dart';
 import 'package:eliza/interceptor.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:connectrpc/protobuf.dart';
-import 'package:connectrpc/protobuf.dart' as protobuf;
-import 'package:connectrpc/protobuf.dart';
-import 'package:connectrpc/protocol/grpc.dart' as grpc;
-
 import 'package:flutter/foundation.dart';
 
 part 'grpc_transport.g.dart';
@@ -31,7 +28,7 @@ Transport grpcTransport(Ref ref) {
     // Protobufの代わりに JSONを使用する場合 ▶️ codec: const JsonCodec()
     codec: const ProtoCodec(), // Protobufを使用する場合
     httpClient: createHttpClient(),
-    statusParser: const protobuf.StatusParser(), //gRPC をトランスポート プロトコルとして使用する場合
+    statusParser: const StatusParser(), //gRPC をトランスポート プロトコルとして使用する場合
     interceptors: [
       const LoggingInterceptor(print),
       const MetadataInterceptor(),
