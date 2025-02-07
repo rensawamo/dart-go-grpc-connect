cf123a29fc7dee6bc0f98a1881ca52dba4eba0fa
rensawamo <ren0112ngzk@gmail.com>
2025-02-07 16:54:37 +0900

    chore: migration dummy dataと setting

--- backend/database/migrations/20220820073520-init.sql
+++ backend/database/migrations/20220820073520-init.sql
@@ -1,1 +1,13 @@
-TODO: proto変更
+-- +migrate Up
+create table sentences (
+    id serial primary key,  
+    sentence text not null
+);
+
+insert into sentences (sentence) values ('Hi');
+insert into sentences (sentence) values ('Hola');
+insert into sentences (sentence) values ('アンニョン');
+insert into sentences (sentence) values ('Bonjour');
+
+-- +migrate Down
+DROP TABLE IF EXISTS sentences;
