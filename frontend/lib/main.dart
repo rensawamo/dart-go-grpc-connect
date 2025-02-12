c99e55c58e0b413c5192f290cde1618754b76979
rensawamo <ren0112ngzk@gmail.com>
2025-02-12 16:19:28 +0900

    feat: logger

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:frontend/app.dart';
+import 'package:frontend/core/util/provider_logger.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   runApp(
     const ProviderScope(
+      observers: [ProviderLogger()],
       child: App(),
     ),
   );
